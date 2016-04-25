de5db8a4ff12f6641f958ab1c3c183a95e0cddfc
PabloFederico <pablo.gabriel.federico@gmail.com>
2016-04-24 21:27:49 -0300

    cambio

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var app = express();
 var pg = require('pg');
 var config = require('./config');
 var bodyParser = require('body-parser');
-var $ = require('jQuery');
 
 //Set: Puerto
 app.set('port', (process.env.PORT || 3000));
