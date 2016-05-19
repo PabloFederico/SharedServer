5c2c7596435e6a1caab86ca012a1bc160e496e7e
fjfarina <74123698Fede>
2016-05-18 22:53:25 -0300

    Migration to express 4

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var pg = require('pg');
 var config = require('./config');
+var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 
 //Set: Puerto
@@ -9,13 +10,10 @@ app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.configure(function() {
-  app.use(express.methodOverride());
-  app.use(require('./middleware/cors'));
-  app.use(app.router);
-});
+app.use(methodOverride());
+app.use(require('./middleware/cors'));
 
 // Set: Carpeta Views para las vistas de la api
 app.set('views', __dirname + '/views');
@@ -26,22 +24,24 @@ app.set('view engine', 'ejs');
 
 require('./routes')(app);
 
-app.get('/', function(request, response) {
-	response.render('pages/main.html');
+app.get('/', function (request, response) {
+  response.render('pages/main.html');
 });
 
 // Set: Conecto el puerto con la api
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
 
 pg.defaults.ssl = true;
 
 //Creo la tabla de la base de datos //
-pg.connect(config.DATABASE_URL, function(err, client) {
+pg.connect(config.DATABASE_URL, function (err, client) {
   if (err)
-  	throw err;
+    throw err;
   console.log('Creando tabla en caso de que no existe');
   var query = client.query('CREATE TABLE IF NOT EXISTS usuarios(id SERIAL PRIMARY KEY, name VARCHAR(30), email VARCHAR(30), alias VARCHAR(20), latitud VARCHAR(30), longitud VARCHAR(30))');
-  query.on('end', function() { client.end(); });
+  query.on('end', function () {
+    client.end();
+  });
 });
