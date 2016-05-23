a2be5bceb41578aaa4cf2c631f81ad3a62633340
Nicolás Vazquez <nicolas.vazquez@fdvsolutions.com>
2016-05-23 00:11:59 -0300

    Fix instate sections for creating and listing interests

---
+++ passport.js
@@ -0,0 +1,32 @@
+'use strict';
+var pg = require('pg');
+var _ = require('underscore');
+var config = require('./config');
+var utils = require('./utils');
+
+exports = module.exports = function(app, passport) {
+  var LocalStrategy = require('passport-local').Strategy;
+
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+
+      pg.connect(config.DATABASE_URL, function (err, client) {
+        client.query("SELECT * FROM users WHERE alias = ($1)", [username], function(err, result) {
+          if (err) {
+            return done(err);
+          }
+
+          if (!result.rowCount) {
+            return done(null, false, { message: 'Unknown user' });
+          }
+
+          if (result.rows[0].password !== utils.encryptPassword(password)) {
+            return done(null, false, { message: 'Invalid password' });
+          }
+
+          return done(null, result.rows[0]);
+        });
+      });
+    }
+  ));
+};
