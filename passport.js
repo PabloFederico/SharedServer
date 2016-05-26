457205b10027887ff45ffdc2718c1f70ffe5a2b1
fjfarina <74123698Fede>
2016-05-26 00:46:21 -0300

    Client end added in every login

--- passport.js
+++ passport.js
@@ -4,23 +4,24 @@ var _ = require('underscore');
 var config = require('./config');
 var utils = require('./utils');
 
-exports = module.exports = function(app, passport) {
+exports = module.exports = function (app, passport) {
   var LocalStrategy = require('passport-local').Strategy;
 
   passport.use(new LocalStrategy(
-    function(username, password, done) {
+    function (username, password, done) {
       pg.connect(config.DATABASE_URL, function (err, client) {
-        client.query("SELECT * FROM users WHERE alias = ($1)", [username], function(err, result) {
+        client.query("SELECT * FROM users WHERE alias = ($1)", [username], function (err, result) {
+          client.end();
           if (err) {
             return done(err);
           }
 
           if (!result.rowCount) {
-            return done(null, false, { message: 'Unknown user' });
+            return done(null, false, {message: 'Unknown user'});
           }
 
           if (result.rows[0].password !== utils.encryptPassword(password)) {
-            return done(null, false, { message: 'Invalid password' });
+            return done(null, false, {message: 'Invalid password'});
           }
 
           return done(null, result.rows[0]);
@@ -29,13 +30,14 @@ exports = module.exports = function(app, passport) {
     }
   ));
 
-  passport.serializeUser(function(user, done) {
+  passport.serializeUser(function (user, done) {
     done(null, user.id);
   });
 
-  passport.deserializeUser(function(id, done) {
+  passport.deserializeUser(function (id, done) {
     pg.connect(config.DATABASE_URL, function (err, client) {
-      client.query("SELECT * FROM users WHERE id = ($1)", [id], function(err, result) {
+      client.query("SELECT * FROM users WHERE id = ($1)", [id], function (err, result) {
+        client.end();
         if (err) {
           return done(err);
         }
