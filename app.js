741b2cf149b22cd82c8ba949df15d17f39bb9758
fjfarina <74123698Fede>
2016-06-14 23:14:27 -0300

    Candidates flow working

--- app.js
+++ app.js
@@ -29,39 +29,52 @@ app.passport = passport;
 require('./routes')(app);
 
 app.get('/', function (request, response) {
-	response.render('main.html');
+  response.render('main.html');
 });
 
 app.listen(app.get('port'), function () {
-	console.log('Node app is running on port', app.get('port'));
+  console.log('Node app is running on port', app.get('port'));
 });
 
 pg.defaults.ssl = true;
 
 pg.connect(config.DATABASE_URL, function (err, client, done) {
-	if (err)
-		throw err;
-	//client.query('DROP TABLE IF EXISTS users',function(err,result){
-	client.query('CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, name VARCHAR(30), alias VARCHAR(20),' +
-		' password VARCHAR(300), email VARCHAR(30), sex VARCHAR(4), age VARCHAR(10),' +
-		' latitude VARCHAR(30), longitude VARCHAR(30), photo_profile bytea)', function (err, result) {
-		if (err) {
-			console.log(err);
-			throw err;
-		}
-		client.query('CREATE TABLE IF NOT EXISTS interests(id SERIAL PRIMARY KEY, category VARCHAR(30), value VARCHAR(30))', function (err, result) {
-			if (err) {
-				console.log(err);
-				throw err;
-			}
-		});
-		//client.query('DROP TABLE IF EXISTS userInterests',function(err,result){
-		client.query('CREATE TABLE IF NOT EXISTS userInterests(id SERIAL PRIMARY KEY, userId VARCHAR(30), interestId VARCHAR(30))', function (err, result) {
-			if (err) {
-				console.log(err);
-				throw err;
-			}
-			done();
-		});//});
-	});//});
+  if (err)
+    throw err;
+  client.query('CREATE TABLE IF NOT EXISTS' +
+  ' users(id SERIAL PRIMARY KEY,' +
+  ' name VARCHAR(30),' +
+  ' alias VARCHAR(20),' +
+  ' password VARCHAR(300),' +
+  ' email VARCHAR(30),' +
+  ' sex VARCHAR(4),' +
+  ' age VARCHAR(10),' +
+  ' latitude VARCHAR(30),' +
+  ' longitude VARCHAR(30),' +
+  ' photo_profile bytea)', function (err, result) {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    client.query('CREATE TABLE IF NOT EXISTS' +
+    ' interests(id SERIAL PRIMARY KEY,' +
+    ' category VARCHAR(30),' +
+    ' value VARCHAR(30))', function (err, result) {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+    });
+
+    client.query('CREATE TABLE IF NOT EXISTS' +
+    ' userInterests(userId integer REFERENCES users (id),' +
+    ' interestId integer REFERENCES interests (id),' +
+    ' PRIMARY KEY(userId, interestId))', function (err, result) {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+      done();
+    });
+  });
 });
